70b07152dd2ac3bdcc14c97ab70ceaaad90b7de4
Paolo Giovannini <giovannini13@gmail.com>
2023-03-31 17:46:27 +0200

    milestone 2 concluso

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -12,10 +12,14 @@ export default{
 
 <template>
     <div>
-        <p>{{ title }}</p>
-        <p v-if="original_title !== title">{{ original_title }}</p>
-        <p>{{ original_language}}</p>
-        <p>{{ vote }}</p>
+        <p>titolo: {{ title }}</p>
+        <p v-if="original_title !== title">titolo originale:{{ original_title }}</p>
+        <img v-if="original_language == 'it'" src="https://flagsapi.com/IT/flat/64.png" alt="it">
+        <img v-else-if="original_language == 'en'" src="https://flagsapi.com/GB/flat/64.png" alt="us">
+        <img v-else-if="original_language == 'de'" src="https://flagsapi.com/DE/flat/64.png" alt="de">
+        <img v-else-if="original_language == 'ja'" src="https://flagsapi.com/JP/flat/64.png" alt="jp">
+        <p v-else>original language: {{ original_language }}</p>
+        <p>voto medio: {{ vote }}</p>
     </div>
 
 
@@ -24,5 +28,7 @@ export default{
 
 <style scoped lang="scss">
 
-
+img{
+    width: 20px;
+}
 </style>
