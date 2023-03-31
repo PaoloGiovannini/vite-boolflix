f45988bf73289652b847b791ddf9806027e3d91d
Paolo Giovannini <giovannini13@gmail.com>
2023-03-31 14:38:19 +0200

    chiamata axios film tramite search e bottone

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <script>
+import axios from 'axios';
+import{ store } from './store.js';
+import MyHeader from './components/MyHeader.vue';
 
+export default{
+  components:{
+    MyHeader
+  },
+    data() {
+        return {
+            store
+        };
+    },
+    methods: {
+      getFilm(){
+        
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=79fa47aa93f64aa97d20631a74952dff&query=${store.search}&language=it-IT`)
+        .then(response => {
+          this.store.filmArray = response.data.results;
+          
+        });
+      }
+    },
+    created(){
+      this.getFilm();
+    }
+    
+}
 </script>
 
 <template>
+
+  <MyHeader @doSearch="getFilm"/>
 
 </template>
 
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,43 @@
+<script>
+import{ store } from '../store.js';
+
+export default{
+    name: 'MyHeader',
+    data(){
+        return{
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="d-flex justify-between" id="my-header">
+        <h2>BOOLFLIX</h2>
+        <div>
+            <input type="search" placeholder="cerca film o serie" v-model="store.search">
+            <button @click="$emit('doSearch')">Cerca</button>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+#my-header{
+    background-color: black;
+    padding: 20px;
+    & input{
+        padding: 5px 20px;
+        margin-right: 7px;
+    }
+    & button{
+        padding: 7px 10px;
+        background-color: red;
+        border: 1px solid black;
+    }
+    & h2{
+        color: red;
+    }
+}
+</style>
